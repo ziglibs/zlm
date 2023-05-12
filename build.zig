21c1ec226d3fd244460270f1dfc9ab0eace01926
Felix "xq" Queißner <xq@random-projects.net>
2023-05-12 13:16:35 +0200

    Adds module system.

---
+++ build.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    _ = b.addModule("zlm", .{
+        .source_file = .{ .path = "src/zlm.zig" },
+    });
+
+    const test_exe = b.addTest(.{
+        .root_source_file = .{ .path = "src/test.zig" },
+    });
+
+    const test_run = b.addRunArtifact(test_exe);
+
+    b.getInstallStep().dependOn(&test_run.step);
+}
--- zig.mod
+++ zig.mod
@@ -1,5 +1,5 @@
 id: al1d3deiv60z0047a68p4vgwp1laitio6n2bp777quwu4xyc
 name: zlm
-main: zlm.zig
+main: src/zlm.zig
 license: MIT
 description: Zig linear mathemathics
