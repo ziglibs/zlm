89d21c163dd1646e926e6b3df27be6d399bc159f
Angelo 'Flecart' Huang <xuanqiang.huang@studio.unibo.it>
2023-09-23 12:33:22 +0200

    refactor: add space

--- src/zlm-generic.zig
+++ src/zlm-generic.zig
@@ -571,7 +571,7 @@ pub fn SpecializeOn(comptime Real: type) type {
                 result.fields[1][1] = 1.0 / (tanHalfFovy);
                 result.fields[2][2] = - (far + near) / (far - near);
                 result.fields[2][3] = - 1;
-                result.fields[3][2] = -(2 * far * near) / (far - near);
+                result.fields[3][2] = - (2 * far * near) / (far - near);
                 return result;
             }
 
