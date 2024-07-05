35b76fae19f9d0a1ed541db621d7c4b3e266087e
MCRusher <Modernwarfare3Minecraft64@gmail.com>
2024-07-05 07:12:05 -0400

    Update build.zig to work with newer zig versions

--- build.zig
+++ build.zig
@@ -2,11 +2,11 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("zlm", .{
-        .root_source_file = .{ .path = "src/zlm.zig" },
+        .root_source_file = b.path("src/zlm.zig"),
     });
 
     const test_exe = b.addTest(.{
-        .root_source_file = .{ .path = "src/test.zig" },
+        .root_source_file = b.path("src/test.zig"),
     });
 
     const test_run = b.addRunArtifact(test_exe);
