4056872ada36fc7a8d285779ec488dcb95c69e08
kdx <kikoodx@paranoici.org>
2025-08-29 13:59:15 +0200

    update for zig 0.15.1 compatibility

--- build.zig
+++ build.zig
@@ -1,12 +1,21 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     _ = b.addModule("zlm", .{
         .root_source_file = b.path("src/zlm.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
     const test_exe = b.addTest(.{
-        .root_source_file = b.path("src/test.zig"),
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/test.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     const test_run = b.addRunArtifact(test_exe);
--- src/zlm.zig
+++ src/zlm.zig
@@ -1,16 +1,1 @@
-const std = @import("std");
-
 pub const SpecializeOn = @import("zlm-generic.zig").SpecializeOn;
-
-/// Converts degrees to radian
-pub fn toRadians(deg: anytype) @TypeOf(deg) {
-    return std.math.pi * deg / 180.0;
-}
-
-/// Converts radian to degree
-pub fn toDegrees(rad: anytype) @TypeOf(rad) {
-    return 180.0 * rad / std.math.pi;
-}
-
-// export all vectors by-default to f32
-pub usingnamespace SpecializeOn(f32);
